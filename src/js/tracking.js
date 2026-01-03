6fd63c35072eb94913a3468df17436f84b55c734
goodstaff <code@goodstaff.at>
2026-01-03 21:09:54 +0100

    refactor: Centralize Matomo tracking logic into `tracking.js` and update Matomo domain.

--- src/js/tracking.js
+++ src/js/tracking.js
@@ -1,8 +1,11 @@
-var idSite = 1;
-var matomoTrackingApiUrl = "//matomo.deinmentor.at/piwik.php";
-
-var _paq = window._paq = window._paq || [];  
-_paq.push(['setTrackerUrl', matomoTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+(function () {
+    var u = "//matomo.goodstaff.at/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+})();
